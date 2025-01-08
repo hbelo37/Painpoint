0eaed619bee6f359b700f9681c028cae611026b4
hbelo37 <142715751+hbelo37@users.noreply.github.com>
2025-01-08 21:40:32 +0530

    Fix indentation in run.py

--- run.py
+++ run.py
@@ -3,4 +3,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)  # Changed to port 8080
+    app.run(host='0.0.0.0', port=10000)
+else:
+    application = app  # This line is important for gunicorn
